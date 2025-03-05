e726f83fadf939830b6a6d52e2a0816820b37316
marilynmansonrare <marilynmansonrare@gmail.com>
2025-03-05 13:13:14 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-document.getElementById('passwordForm').addEventListener('submit', function(e) {
+
+document.getElementById("passwordForm").onsubmit = function(e) {
   e.preventDefault();
-  const password = document.getElementById('password').value.toUpperCase();
-  
-  if (password === 'VII') {
-    document.getElementById('message').classList.remove('hidden');
+  const password = document.getElementById("password").value.toUpperCase();
+  if (password === "VII") {
+    document.getElementById("message").style.display = "block";
     setTimeout(() => {
-      document.getElementById('coordinates').classList.remove('hidden');
+      document.getElementById("coordinates").style.display = "block";
     }, 3000);
   } else {
-    alert('WRONG PASSWORD');
+    alert("Wrong password.");
   }
-});
+};
